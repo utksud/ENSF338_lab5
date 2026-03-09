0f27519db4c6a0c062976c7050ce8207837d1754
Utkarsh Sudhir <128128195+utksud@users.noreply.github.com>
2026-03-09 10:55:31 -0600

    Implement Stack class with basic operations

---
+++ ex1.py
@@ -0,0 +1,21 @@
+import sys
+
+class Stack:
+    def __init__(self):
+        self.items = []
+
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        if not self.is_empty():
+            return self.items.pop()
+        return None
+
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        return None
+
+    def is_empty(self):
+        return len(self.items) == 0
